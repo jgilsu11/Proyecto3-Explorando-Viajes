60eadb13ece8e1065eba48e855b6b95b1250786e
jgilsu11 <jaimegilnero@gmail.com>
2024-10-19 15:25:32 +0200

    Subida con funciones de hotel y vuelo pasadas a .py

--- src/funciones.py
+++ src/funciones.py
@@ -97,3 +97,82 @@ def crearDF_formateado(funcion_info_vuelos):
     df_vuelos_ida_vuelta["salida"]= pd.to_datetime(df_vuelos_ida_vuelta["salida"])
     df_vuelos_ida_vuelta["llegada"]= pd.to_datetime(df_vuelos_ida_vuelta["llegada"])
     return df_vuelos_ida_vuelta
+
+
+
+def info_hoteles(request_jeison):
+    """
+    Extrae información relevante sobre hoteles a partir de una respuesta JSON de un servicio de búsqueda de hoteles.
+
+    La función itera sobre los resultados del JSON y extrae información clave sobre cada hotel, 
+    incluyendo el nombre, número de reseñas, rating, rango de precios, ciudad, servicios ofrecidos y un enlace al hotel.
+    Calcula además el precio medio por noche basado en el rango de precios.
+
+    Args:
+        request_jeison (dict): Un diccionario que representa la respuesta en formato JSON de un servicio de búsqueda de hoteles.
+
+    Returns:
+        list[dict]: Una lista de diccionarios, donde cada diccionario contiene la información de un hotel con las siguientes claves:
+            - nombre_hotel: El nombre del hotel.
+            - reviews_hotel: Número de reseñas del hotel.
+            - rating_hotel: Rating del hotel.
+            - precio_rango_min: El precio mínimo por noche en USD.
+            - precio_rango_max: El precio máximo por noche en USD.
+            - precio_medio_por noche: Precio medio por noche calculado a partir del rango de precios.
+            - ciudad: Ciudad donde se encuentra el hotel.
+            - servicios: Lista de servicios que ofrece el hotel.
+            - link: URL que enlaza a la página del hotel.
+    """
+    lista_dicch=[]
+    respond=request_jeison
+    for i in range(0,len(respond["results"])):
+
+        nombre_hotel=respond["results"][i]["name"]
+        reviews_hotel=respond["results"][i]["reviews"]
+        rating_hotel=respond["results"][i]["rating"]
+        precio_rango_min=respond["results"][i]["price_range_usd"]["min"]
+        precio_rango_max=respond["results"][i]["price_range_usd"]["max"]
+        precio_medio= (precio_rango_max + precio_rango_min)/2
+        ciudad=respond["results"][i]["detailed_address"]["city"]
+        servicios=respond["results"][i]["amenities"]
+        link=respond["results"][i]["link"]
+
+        
+        dicch= {"nombre_hotel":nombre_hotel,
+                "reviews_hotel":reviews_hotel,
+                "rating_hotel": rating_hotel,
+                "precio_rango_min":precio_rango_min,
+                "precio_rango_max":precio_rango_max,
+                "precio_medio_por noche":precio_medio,
+                "ciudad":ciudad,
+                "servicios":servicios,
+                "link":link
+                }
+        lista_dicch.append(dicch)
+    return lista_dicch
+        
+
+
+
+
+
+
+def crearDF_formateado_hoteles(funcion_info_hoteles):
+    """
+    Crea y devuelve un DataFrame formateado a partir de la información de hoteles obtenida.
+
+    La función recibe una lista de diccionarios generada por la función `info_hoteles`, 
+    convierte esa lista en un DataFrame y elimina las columnas "precio_rango_min" y "precio_rango_max",
+    manteniendo solo la información relevante para el análisis.
+
+    Args:
+        funcion_info_hoteles (list[dict]): Lista de diccionarios con información sobre hoteles, 
+        obtenida de la función `info_hoteles`.
+
+    Returns:
+        pd.DataFrame: Un DataFrame con los datos de hoteles, excluyendo las columnas "precio_rango_min" y "precio_rango_max".
+    """
+
+    df_hotel= pd.DataFrame(funcion_info_hoteles).drop(columns= ["precio_rango_min", "precio_rango_max"])
+    df_hotel
+    return df_hotel
